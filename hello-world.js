01e686c0682e01f86a3a3451f2e484278cd70864
erickalvdev <alvaradoerick26@gmail.com>
2023-09-08 10:21:47 -0300

    Apresentação

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,5 @@
-console.log("Hello World")
+console.log("Hello World");
+const nome = 'Erick';
+const curso = "desenvolvimento de sofware com foco em Back-End";
+const instituto = 'Cubos academy'
+console.log(`Meu nome é ${nome} e estou estudando ${curso} na ${instituto}`);
